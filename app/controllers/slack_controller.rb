d6f34cfef3ea114d94c16ec583ad9aeefb65a446
Andrew Arrow <oneone@gmail.com>
2017-01-25 23:54:17 +0000

    route

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,6 +1,6 @@
 class SlackController < ApplicationController
 
-  def slack
+  def auth
        #{"ok":true,"access_token":"6f","scope":"identify,bot","user_id":"U1","team_name":"higher.team","team_id":"TL","bot":{"bot_user_id":"UP","bot_access_token":"zg"}} 
 
     code = params[:code]
@@ -23,5 +23,28 @@ class SlackController < ApplicationController
       puts "ERROR: could not parse response"
     end
     redirect_to '/'
+  end
+
+  def event
+    data = JSON.parse(request.raw_post)
+    #puts data.inspect
+    team_id = data["team_id"]
+
+    b=Bot.where(team_id: team_id).order(:id).last
+
+    return unless b
+
+    et = data["event"]["type"]
+    from = data["event"]["user"]
+    bot_id = data["event"]["bot_id"]
+    subtype = data["event"]["subtype"]
+    text = data["event"]["text"]
+    chan = data["event"]["channel"]
+    ts = data["event"]["ts"].to_f
+
+    return if bot_id == b.bot_id
+    return if subtype == "bot_message"
+
+    client = Slack::Client.new(token: b.bot_token)
   end
 end
